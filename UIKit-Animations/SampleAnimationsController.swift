aa53a5ecdccc8de36353646b17180d0b57da47cf
Matthew Ramos <mmramos309@gmail.com>
2020-01-31 12:09:08 -0500

    Set up basic pulse and transforming animations on first image view in vc

--- UIKit-Animations/SampleAnimationsController.swift
+++ UIKit-Animations/SampleAnimationsController.swift
@@ -9,9 +9,39 @@
 import UIKit
 
 class SampleAnimationsController: UIViewController {
-
+    
+    private let sampleAnimationsView = SampleAnimationView()
+    
+    override func loadView() {
+        view = sampleAnimationsView
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        pulsatingAnimation()
+    }
+    
+    private func scaleAnimation() {
+        UIView.animate(withDuration: 1.5) { // 1/3 of a second
+            self.sampleAnimationsView.pursuitLogo.transform = CGAffineTransform(scaleX: 10.0, y: 10.0)
+            self.sampleAnimationsView.pursuitLogo.alpha = 0.0
+        }
+    }
+    
+    private func pulsatingAnimation() {
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            self.sampleAnimationsView.pursuitLogo.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) { (done) in
+            UIView.animate(withDuration: 0.3) {
+                self.sampleAnimationsView.pursuitLogo.transform = CGAffineTransform.identity
+            }
+            //if you want to go beyond creating an animation in this completion handler. the better choice would be animateKeyFrames()
+        }
     }
 }
+
