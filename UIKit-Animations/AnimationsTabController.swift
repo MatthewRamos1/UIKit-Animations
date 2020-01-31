e1a64188e05174953975f9bdb69fe5b7cbdb2263
Matthew Ramos <mmramos309@gmail.com>
2020-01-31 10:53:42 -0500

    Created instances of view controllers programmatically to be loaded into tab bar controller

--- UIKit-Animations/AnimationsTabController.swift
+++ UIKit-Animations/AnimationsTabController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class AnimationsTabController: UITabBarController {
+    
+    private lazy var sampleAnimationsVC: SampleAnimationsController = {
+        let viewController = SampleAnimationsController()
+        viewController.tabBarItem = UITabBarItem(title: "Sample Animations", image:
+            UIImage(systemName: "1.circle"), tag: 0)
+        
+        return viewController
+    }()
+    
+    private lazy var constraintsAnimationVC: ConstraintsAnimationController = {
+        let viewController = ConstraintsAnimationController()
+        viewController.tabBarItem = UITabBarItem(title: "Constraints Animations", image: UIImage(systemName: "2.circle"), tag: 1)
+        return viewController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewControllers = [SampleAnimationsController(), ConstraintsAnimationController()]
+        viewControllers = [sampleAnimationsVC, constraintsAnimationVC]
     }
 }
